da552c94c4c5f279e5216f5b5f168f083add2f6f
Kuddleman <edl1155@gmail.com>
2020-12-29 17:35:19 -0800

    add styling to component

--- src/index.js
+++ src/index.js
@@ -3,34 +3,30 @@
         
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay'
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  
-  state = { lat: null, errorMessage: '' }
-
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })  
-    )
+      err => this.setState({ errorMessage: err.message })
+    );
   }
 
-
+  // React says we have to define render!!
   render() {
-           if (this.state.errorMessage && !this.state.lat) {
-             return <div>Error: {this.state.errorMessage}</div>
-           } 
-           if (!this.state.errorMessage && this.state.lat){
-             return <SeasonDisplay lat={this.state.lat}/>
-           }
-           
-             return <div>Loading...</div>      
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
-
-
 ReactDOM.render(<App />, document.querySelector('#root'));
-
